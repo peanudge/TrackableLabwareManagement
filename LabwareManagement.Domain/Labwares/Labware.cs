9cdc1d54501b4f1da89dc032b8bb46683bd5851f
peanut-lover <jihoson94@gmail.com>
2025-01-20 23:53:15 +0900

    Remove unused Sample Projects

--- LabwareManagement.Domain/Labwares/Labware.cs
+++ LabwareManagement.Domain/Labwares/Labware.cs
@@ -6,8 +6,6 @@ public enum LabwareUseStatus
     InUse = 2,
 }
 
-
-// Labware is state machine, Labware move from one status to another through life cycle of the lab.
 public class Labware
 {
     private LabwareUseStatus _status;
